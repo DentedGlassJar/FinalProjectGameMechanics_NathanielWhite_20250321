803001a54b57e5e69a94bac91197cbe8537c12cc
DentedGlassJar <nathanielwhite867991@gmail.com>
2025-03-21 05:03:01 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    Vector3 spawnPoint;
+    public AudioSource catHurt;
+
+    int sceneIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoint = new Vector3(-6.4f, 7.5f, 0f);
+
+        sceneIndex = 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Buzzsaw"))
+        {
+            Debug.Log("Player has been moved back!");
+            catHurt.Play();
+            gameObject.transform.position = spawnPoint;     
+        }
+
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            Debug.Log($"Scene is now {sceneIndex}");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static InputManager;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D playerRigidbody;
+
+    private Animator playerWalkFloat;
+
+    public Vector2 moveDirection;
+    private float moveSpeed = 4f;
+    private bool hasDashed;
+    private float DashCooldown = 5f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = this.GetComponent<Rigidbody2D>();
+
+        playerWalkFloat = this.GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        Actions.moveEvent += UpdateVector;
+    }
+
+    private void OnDisable()
+    {
+        Actions.moveEvent -= UpdateVector;
+    }
+
+    void HandlePlayerMovement()
+    {
+        playerRigidbody.MovePosition(playerRigidbody.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
+
+        if (Input.GetKeyDown(KeyCode.RightShift) && Input.GetKeyDown(KeyCode.S) && hasDashed == false)
+        {
+            gameObject.transform.Translate(0f, -2f, 0);
+            hasDashed = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightShift) && Input.GetKeyDown(KeyCode.W) && hasDashed == false)
+        {
+            gameObject.transform.Translate(0f, 2f, 0);
+            hasDashed = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightShift) && Input.GetKeyDown(KeyCode.D) && hasDashed == false)
+        {
+            gameObject.transform.Translate(2f, 0f, 0);
+            hasDashed = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightShift) && Input.GetKeyDown(KeyCode.A) && hasDashed == false)
+        {
+            gameObject.transform.Translate(-2f, 0f, 0);
+            hasDashed = true;
+        }
+    }
+
+    // if the MoveEvent is enabled, it updates the moveVector value with the inputVector value
+    void UpdateVector(Vector2 inputVector)
+    {
+        moveDirection = inputVector;
+    }
+
+
+    private void FixedUpdate()
+    {
+        HandlePlayerMovement();
+    }
+
+    private void Update()
+    {
+        playerWalkFloat.SetFloat("X", moveDirection.x);
+
+        if (hasDashed == true)
+        {
+            Debug.Log("PLayer is on cooldown!");
+            DashCooldown = DashCooldown - 0.05f;
+
+            if (DashCooldown <= 0)
+            {
+                DashCooldown = 5f;
+                Debug.Log("Player is able to dash again");
+                hasDashed = false;
+            }
+        }
+    }
+}
